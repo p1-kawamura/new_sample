638933dee85a00cd55695039cf5d59b3c5c9b519
kawamura <lailai666@hotmail.com>
2023-05-09 19:54:00 +0900

    desk1953

--- new_sample/new_sample/settings.py
+++ new_sample/new_sample/settings.py
@@ -133,8 +133,3 @@ LOGOUT_REDIRECT_URL = 'zaiko:login'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# メール
-EMAIL_HOST = 'smtp.p1-intl.com'
-EMAIL_HOST_USER = 'kawamura@p1-intl.com'
-EMAIL_HOST_PASSWORD = 'G2Y3&Ugz'
-EMAIL_PORT = '587' #メールサーバーで指定されているポート
